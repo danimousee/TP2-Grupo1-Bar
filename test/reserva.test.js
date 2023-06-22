5eb823ce9c29dc63efc976ecc3867dde646abb2c
Nati <galetta.natalia@gmail.com>
2023-06-21 21:33:08 -0300

    reserva.test.js

--- test/reserva.test.js
+++ test/reserva.test.js
@@ -1,9 +1,19 @@
 import { expect } from 'chai'
 import generador from './generador/reserva.js'
 
-describe('Deberia generar una reserva aleatoria'), () => {
-const reserva = generador.get()
-console.log(reserva)
+describe('Test generador de reserva'), () => {
+it('La reserva deberia poseer campos nombre, cantidad de personas', () => {
+    const reserva = generador.get()
 
-expect(reserva).to.include.keys('nombre', 'cantidad_personas')
+    expect(reserva).to.include.keys('nombre', 'cantidad_personas')
+})
+
+it('Deberia generar reservas con datos aleatorios', () => {
+    const reser1 = generador.get()
+    const reserv2 = generador.get()
+
+    expect(reser1.nombre).not.to.eql(reserv2.nombre)
+    expect(reserv1.cantidad_personas).not.to.eql(reserv2.cantidad_personas)
+
+})
 }
