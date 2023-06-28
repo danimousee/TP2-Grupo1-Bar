173b85801be9c534c84542c578d36219f71742d7
Christian Viarnes <96167988+christiancdv@users.noreply.github.com>
2023-06-28 13:15:28 -0300

    sucursales en home

--- Bar_Grupo1/src/components/Home.jsx
+++ Bar_Grupo1/src/components/Home.jsx
@@ -6,6 +6,8 @@ const Home = () => {
         <Boton name={"Nuestra Carta"} url= {"/LaMixtureria/Carta"} />
         <br></br>
         <Boton name={"Reservas"} url= {"/LaMixtureria/Reservas"} />
+        <br></br>
+        <Boton name={"Sucursales"} url= {"/LaMixtureria/Sucursales"} />
       </div>
     )
   }
