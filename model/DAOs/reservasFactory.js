67b4c20ca9c7a917a864270b251700d6f52e76af
Veronica Gomez <veronicalauragomez@gmail.com>
2023-06-27 21:26:59 -0300

    Se arregla conexion a la base de datos Mongo y se eliminan archivos de FS

--- model/DAOs/reservasFactory.js
+++ model/DAOs/reservasFactory.js
@@ -1,12 +1,12 @@
 import ModelMongoDB from "./reservasMongoDB.js";
-import ReservasFS from "./reservasFS.js";
+//import ReservasFS from "./reservasFS.js";
 import { config } from "dotenv";
 
 
 class ReservasFactoryDAO {
     static get(tipo) {
         switch(tipo) {
-            case 'FILE': return new ReservasFS(config.FILE)
+            //case 'FILE': return new ReservasFS(config.FILE)
             case 'MONGODB': return new ModelMongoDB(config.MONGODB)
                 }
     }
--- model/DBMongo.js
+++ model/DBMongo.js
@@ -1,4 +1,5 @@
 import { MongoClient }  from "mongodb";
+import config from "../config.js";
 
 class CnxMongoDB {
     static client = null
